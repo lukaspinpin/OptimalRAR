6e66a854ee7d986169632d862a5f15ea4355eef9
Lukas Pin <44980321+lukaspinpin@users.noreply.github.com>
2024-01-30 15:12:11 +0000

    Add files via upload

---
+++ ERADE.R
@@ -0,0 +1,170 @@
+
+###############################################################################
+library(BSDA)
+library(rankFD)
+library(pseudorank)
+
+source('Data_Generator.R')
+
+sim_ERADE <-  function(N=200, dist="bern", K=1, par1 = c(0,0), par2 = NULL, measure="sd", burnin=3, ar="WMW",nsim=10^4, alpha=0.05, one.sided = FALSE){
+  
+  if(K==1){
+    out = matrix(nrow = nsim, ncol = 7)
+    for(i in 1:nsim){
+      if(i==1){
+        out[i,] = two_arm_ERADE(N=N, dist=dist,  par1 =par1, par2 = par2, measure=measure, burnin = burnin,ar=ar, first=FALSE)
+      } else {
+        out[i,] = two_arm_ERADE(N=N, dist=dist,  par1 =par1, par2 = par2, measure=measure, burnin = burnin,ar=ar)
+      }
+      
+    }
+  } else {
+    out = matrix(nrow = nsim, ncol = 6+K)
+  }
+  return(out)
+}
+
+
+
+###############################################################################
+
+two_arm_ERADE = function(N=200, dist="bern",  par1 = c(0,0), par2 = NULL, measure="sd", burnin=3, ar="WMW", alphaE=0.5, Z_corrected=FALSE, first=FALSE){
+  
+  A = rep(NA, N)   # Vector of allocations
+  X = rep(NA, N)   # Vector of responses
+  est <- rep(NA, N) 
+  
+  #Burnin
+  if(burnin>0){
+    A[1:burnin] = 1
+    X[1:burnin] = generate_data(N=burnin, dist=dist, parV=c(par1[1],par2[1]))
+    A[(burnin+1):(2*burnin)] = 2
+    X[(burnin+1):(2*burnin)] = generate_data(N=burnin, dist=dist, parV=c(par1[2],par2[2]))
+    est[1:(2*burnin)] <- 0.5
+  }
+  
+  for (i in (2*burnin+1):N){
+    n1 = sum(A==1, na.rm = TRUE)
+    n2 = sum(A==2, na.rm = TRUE)
+    
+    if(ar=="R_minF"){
+      if(dist=="bern") {
+        s1 = sum(X[A==1], na.rm = TRUE)
+        p1.hat = (s1)/(n1)
+        s2 = sum(X[A==2], na.rm = TRUE)
+        p2.hat = (s2)/(n2)
+        if(measure=="sd"){
+          if((sqrt(p1.hat) + sqrt(p2.hat)) == 0){
+            rho1.hat = 0.5
+          } else {
+            rho1.hat = sqrt(p1.hat)/(sqrt(p1.hat) + sqrt(p2.hat))
+          }
+        } else if(measure=="lrr"){
+          if((sqrt(p1.hat)*(1-p2.hat) + sqrt(p2.hat)*(1-p1.hat)) == 0){
+            rho1.hat = 0.5
+          } else {
+            rho1.hat = 1- sqrt(p2.hat)*(1-p1.hat) / (sqrt(p1.hat)*(1-p2.hat) + sqrt(p2.hat)*(1-p1.hat))
+          }
+        }
+        p = 1-rho1.hat
+      }
+    }
+    if(ar=="Neyman"){
+      if(measure=="sd"){
+        sig1_N <- sd(X[A==1], na.rm = TRUE)
+        sig2_N <- sd(X[A==2], na.rm = TRUE)
+        if((sig1_N+sig2_N)==0){
+          p <- 0.5
+        } else {
+          p <- 1 - sig1_N/(sig1_N+sig2_N)
+        }
+      } else if(measure=="lrr"){
+        s1 = sum(X[A==1], na.rm = TRUE)
+        p1.hat = (s1)/(n1)
+        s2 = sum(X[A==2], na.rm = TRUE)
+        p2.hat = (s2)/(n2)
+        if((sqrt((1-p1.hat)*p2.hat) + sqrt((1-p2.hat)*p1.hat))==0){
+          p <- 0.5
+        } else {
+          p <- sqrt((1-p1.hat)*p2.hat) / (sqrt((1-p1.hat)*p2.hat) + sqrt((1-p2.hat)*p1.hat))
+        }
+      }
+      est[i] <- p
+    }
+    if(p==0){p <- 1/N}
+    if(p==1){p <- 1-1/N}
+    est[i] <- p
+    alloc.prop = n1/i
+    
+    phi1 = ERADE(1-p, alloc.prop, alphaE) #USE AUC here instead 
+    A[i] = rbinom(1, 1, 1-phi1) + 1
+    X[i]  <- generate_data(N=1, dist=dist, parV=c(par1[A[i]],par2[A[i]])) 
+  }
+
+  
+  # Response
+  x0 <- X[A==1]; x1 <- X[A==2]
+  n0 <- length(x0); n1 <- length(x1)
+  s0 <- sum(x0); s1 <- sum(x1)
+  response <- (s0+s1)/N
+  
+  #Imbalance Measure
+  sup <- superior(dist=dist,par1 = par1, par2 = par2)
+  if(sup==2){ #2 means that no arm is theoretically supirior 
+    #   imb <- NA
+    per.sup <- NA
+  } else {
+    if(sup==0){ 
+      #    low <- n1 ; lar <- n0
+      per.sup <- n0/N
+    }
+    if(sup==1){ 
+      #   low <- n0 ; lar <- n1
+      per.sup <- n1/N
+    }
+  }
+  
+  ##### Z - Test & BM-Test
+  source('WaldTest.R')
+  ##### Z - Test 
+  if(dist=="bern"){
+    Z_P <-wald.test.binary(x0,x1, measure = measure)
+  } else {
+    Z_P <- wald.test(x0,x1)
+  }
+  # One-Point-Sample-Correction
+  sigx <- sd(x0); sigy <- sd(x1)
+  if(sigx==0 && sigy==0){
+    BM_P <- Z_P
+  } else {
+    BM <- lawstat::brunner.munzel.test(x0,x1)
+    if(BM$statistic == Inf){
+      BM_P <- 0
+    } else if(BM$statistic == -Inf) {
+      BM_P <-  1
+    } else {
+      BM_P <- BM$p.value
+    }
+  }
+  #estimator <- p
+  n <- c(n0,n1)/N
+  return(c(response, Z_P, BM_P, per.sup, n, p))
+}
+
+
+###############################################################################
+
+ERADE = function(rho.hat, alloc.prop, alpha=0.5){
+  
+  if(alloc.prop > rho.hat){
+    p = alpha*rho.hat
+  } else if(alloc.prop < rho.hat){
+    p = 1 - alpha*(1 - rho.hat)
+  } else{
+    p = rho.hat
+  }
+  
+  return(p)
+}
+
+sim_ERADE(N=200, dist="bern", K=1, par1 = c(0.941,0.991), burnin=20, ar="Neyman",nsim=10^1, measure="lrr")
---
+++ Simulation.R
@@ -0,0 +1,365 @@
+library(foreach)
+library(doParallel)
+library(doRNG)
+library(Rfast)
+
+simulation <- function(N=200, dist="bern", K=1, par1 = c(0,0),  par2 = NULL, burnin=3, ar=NULL, nsim= 10^4, method="ER", measure="sd"){
+  
+  #Generate Result Data Frame
+  colnames <-  c()
+  for(i in 1:ncol(par1)){
+    colnames <- c(colnames, paste("p1", i, sep = "", collapse = NULL))
+  }
+  if(!is.null(par2)){
+    for(i in 1:ncol(par2)){
+      colnames <- c(colnames, paste("p2", i, sep = "", collapse = NULL))
+    }
+  }
+  for(j in 1:(K+1)){
+    colnames <- c(colnames, paste( "n", j-1, sep = "", collapse = NULL))
+    colnames <- c(colnames, paste("Var(n", j-1, ")",sep = "", collapse = NULL))
+  }
+  colnames <- c(colnames, "EMR", "Var_EMR", "Z_Type1", "BM_Type1", "Z_Power", "BM_Power", "Per_Superior", "Var_Superior")
+  if(!is.null(ar)){
+    colnames <- c(colnames, ar)
+    colnames <- c(colnames, paste("BIAS(", ar, ")",sep = "", collapse = NULL))
+  }
+  result <- data.frame(matrix(ncol = length(colnames), nrow = max(nrow(par1),nrow(par2))))
+  colnames(result) <- colnames
+  
+  cl <- parallel::makeCluster(4)  #increase according to the limitations of your cluster if you run it on the cluster
+  doParallel::registerDoParallel(cl)
+  
+  #2-Armed Or Multi-armed
+  ######  2  Armed  ##########################################################
+  if(K==1){
+    ########## Theoretical Variance Calculation ############################
+    if(dist=="bern"){
+      mean <- par1
+      fun <- function(x){x*(1-x)}
+      variance <-  data.frame(lapply(par1,fun))
+      theta <- par1[,2]*(1-par1[,1]) + 0.5*(par1[,1]*par1[,2]+(1-par1[,1])*(1-par1[,2]))
+      psi <- 0.5*theta +0.25
+    }
+    if(dist=="norm"){
+      mean <- par1
+      fun <- function(x){x^2}
+      variance <-  data.frame(lapply(par2,fun))
+      xn <- (mean[,1] - mean[,2])/sqrt(variance[,1]+variance[,2])
+      theta <-  1 - dnorm(xn)
+      psi <- 0.5*theta +0.25
+    }
+    if(dist=="expon"){
+      fun <- function(x){1/x}
+      mean <- data.frame(lapply(par1,fun))
+      fun <- function(x){1/(x^2)}
+      variance <-  data.frame(lapply(par1,fun))
+      theta <-  par1[,1]/(par1[,1]+par1[,2])
+      psi <- 0.5*theta +0.25
+    }
+    if(dist=="beta"){
+      fun <- function(x){5/(x+5)}
+      mean <- data.frame(lapply(par1,fun))
+      fun <- function(x){5*x/((5+x)^2+(5+x+1))}
+      variance <-  data.frame(lapply(par1,fun))
+      theta <- c(0.5,0.5631895, 0.6317465, 0.704089, 0.7777655, 0.813794,0.8486965,
+                 0.881401, 0.91132, 0.937633, 0.959517, 0.976518, 0.988456, 0.991853,
+                  0.994524, 0.995621, 0.997357, 0.998557, 0.998991, 0.999326, 0.999584)
+      psi <- 0.5*theta +0.25
+      psi <- c(rep(0.5, length(theta)),theta[-1])
+    }
+    if(dist=="Lickert"){
+      ## Variance needed to be estimated through simulations in extra Code
+      mean <-  read.csv("Lickert_Mean.csv", header = TRUE)
+      mean <- mean[,2:3]
+      variance <-  read.csv("Lickert_Variance.csv", header = TRUE)
+      variance <- variance[,2:3]
+      theta <- c(0.5, 0.544, 0.593, 0.6446757, 0.696642, 0.7469708, 0.796875, 0.818026,
+                  0.840608, 0.852698, 0.8653635, 0.878488, 0.892124, 0.906136, 0.92029,
+                  0.93435, 0.9479362, 0.9606685, 0.9720625, 0.9817345, 0.9893505,
+                  0.9947455, 0.998026, 0.999626)
+      psi <- 0.5*theta +0.25
+      psi <- c(rep(0.5, length(psi)),psi[-1])
+    }
+    ############## ER ######################################################
+    if(method == "ER" ){
+      result_part_emp <- data.frame(matrix(ncol = length(colnames), nrow = 0))
+      result.ER <- foreach(i = 1:length(par1[,1]), .combine='rbind', .packages=c("BSDA", "rankFD")) %dorng% { #%dorng% 
+        
+        result_part <-result_part_emp
+        
+        source('ER.R')
+        p <- as.numeric(par1[i,])
+        start <- length(p)
+        result_part[1,1:start] <- p
+        if(is.null(par2)){
+          sim.ER <- sim_ER(N=N, dist=dist, K=1, par1 = p,  burnin= burnin, nsim=nsim, measure=measure)
+        } else{
+          sig <- as.numeric(par2[i,])
+          start <- start+ length(sig)
+          result_part[1,1:start] <- c(p, sig)
+          sim.ER <- sim_ER(N=N, dist=dist, K=1, par1 = p, burnin= burnin, par2=sig, nsim=nsim)
+        }
+        
+        
+        result_part[1,(start+1)] <- mean(sim.ER[,5]); result_part[1,(start+2)] <- var(sim.ER[,5]) 
+        result_part[1,(start+3)] <- mean(sim.ER[,6]); result_part[1,(start+4)] <- var(sim.ER[,6]) 
+        
+        result_part[1,(start+5)] <- mean(sim.ER[,1])
+        result_part[1,(start+6)] <- var(sim.ER[,1])
+        
+        reject_Z <- sum(sim.ER[,2] < 0.05)
+        reject_BM <- sum(sim.ER[,3] < 0.05)
+        
+        if(p[1]==p[2]){
+          result_part[1,(start+7)] <- reject_Z/nsim
+          result_part[1,(start+8)] <- reject_BM/nsim
+        } else {
+          result_part[1,(start+7)] <- NA
+          result_part[1,(start+8)] <- NA
+        }
+        result_part[1,(start+9)] <- reject_Z/nsim
+        result_part[1,(start+10)] <- reject_BM/nsim
+        
+        result_part[1,(start+11)] <- mean(sim.ER[,4])
+        result_part[1,(start+12)] <- var(sim.ER[,4])
+        
+        result_part
+        
+      }
+      result[,1:(length(colnames))] <- result.ER
+    }
+    ############## Sequential ######################################################
+    if(method == "SEQ_R_minF"){  ### change
+      result_part_emp <- data.frame(matrix(ncol = length(colnames), nrow = 0))
+      result.SEQ_R <- foreach(i = 1:length(par1[,1]), .combine='rbind', .packages=c("BSDA", "rankFD")) %dorng% { #%dorng% 
+        source('SEQ.R')
+        result_part <-result_part_emp
+        p <- as.numeric(par1[i,])
+        start <- length(p)
+        result_part[1,1:start] <- p
+        sim.SEQ_R <- sim_SEQ(N=N, dist=dist, K=1, par1 = p,  nsim=nsim, measure=measure, ar=ar, burnin=burnin)
+        result_part[1,(start+1)] <- mean(sim.SEQ_R[,5]); result_part[1,(start+2)] <- var(sim.SEQ_R[,5]) 
+        result_part[1,(start+3)] <- mean(sim.SEQ_R[,6]); result_part[1,(start+4)] <- var(sim.SEQ_R[,6]) 
+        
+        result_part[1,(start+5)] <- mean(sim.SEQ_R[,1])
+        result_part[1,(start+6)] <- var(sim.SEQ_R[,1])
+        
+        reject_Z <-  sum(sim.SEQ_R[,2] < 0.05)
+        reject_BM <- sum(sim.SEQ_R[,3] < 0.05)
+        
+        if(p[1]==p[2]){
+          result_part[1,(start+7)] <- reject_Z/nsim
+          result_part[1,(start+8)] <- reject_BM/nsim
+        } else {
+          result_part[1,(start+7)] <- NA
+          result_part[1,(start+8)] <- NA
+        }
+        result_part[1,(start+9)] <- reject_Z/nsim
+        result_part[1,(start+10)] <- reject_BM/nsim
+        
+        result_part[1,(start+11)] <- mean(sim.SEQ_R[,4])
+        result_part[1,(start+12)] <- var(sim.SEQ_R[,4])
+        
+        result_part[1,(start+13)] <- 1- sqrt(p[1])/(sqrt(p[1]) + sqrt(p[2]))
+
+        result_part[1,(start+14)] <- mean(sim.SEQ_R[,7]) - result_part[1,(start+13)]
+        result_part
+      }
+      result[,1:(length(colnames))] <- result.SEQ_R 
+    }
+    if(method == "SEQ_Neyman"){
+      result_part_emp <- data.frame(matrix(ncol = length(colnames), nrow = 0))
+      result.SEQ_Neyman <- foreach(i = 1:length(par1[,1]), .combine='rbind', .packages=c("BSDA", "rankFD")) %dorng% { #%dorng% 
+        source('SEQ.R')
+        result_part <-result_part_emp
+        p <- as.numeric(par1[i,])
+        var <- as.numeric(variance[i,])
+        if(dist=="norm") {
+          std <- as.numeric(par2[i,])
+          start <- length(c(p,std))
+          sim.SEQ_Neyman <- sim_SEQ(N=N, dist=dist, K=1, par1 = p, par2 = std,  nsim=nsim, ar=ar, burnin=burnin)
+          result_part[1,1:start] <- c(p,std)
+        } else {
+          start <- length(p)
+          sim.SEQ_Neyman <- sim_SEQ(N=N, dist=dist, K=1, par1 = p,  measure=measure, nsim=nsim, ar=ar, burnin=burnin)
+          result_part[1,1:start] <- p
+        }
+        result_part[1,(start+1)] <- mean(sim.SEQ_Neyman[,5]); result_part[1,(start+2)] <- var(sim.SEQ_Neyman[,5]) 
+        result_part[1,(start+3)] <- mean(sim.SEQ_Neyman[,6]); result_part[1,(start+4)] <- var(sim.SEQ_Neyman[,6]) 
+        
+        result_part[1,(start+5)] <- mean(sim.SEQ_Neyman[,1])
+        result_part[1,(start+6)] <- var(sim.SEQ_Neyman[,1])
+        
+        reject_Z <-  sum(sim.SEQ_Neyman[,2] < 0.05)
+        reject_BM <- sum(sim.SEQ_Neyman[,3] < 0.05)
+        
+        if(p[1]==p[2]){
+          result_part[1,(start+7)] <- reject_Z/nsim
+          result_part[1,(start+8)] <- reject_BM/nsim
+        } else {
+          result_part[1,(start+7)] <- NA
+          result_part[1,(start+8)] <- NA
+        }
+        result_part[1,(start+9)] <- reject_Z/nsim
+        result_part[1,(start+10)] <- reject_BM/nsim
+        
+        result_part[1,(start+11)] <- mean(sim.SEQ_Neyman[,4])
+        result_part[1,(start+12)] <- var(sim.SEQ_Neyman[,4])
+        
+        result_part[1,(start+13)] <- 1 - var[1]/(var[1]+var[2])
+        result_part[1,(start+14)] <- mean(sim.SEQ_Neyman[,7]) - result_part[1,(start+13)]
+        
+        result_part
+        
+      }
+      result[,1:(length(colnames))] <- result.SEQ_Neyman 
+    }
+    if( method == "ERADE"){
+      result_part_emp <- data.frame(matrix(ncol = length(colnames), nrow = 0))
+      result.ERADE <- foreach(i = 1:length(par1[,1]), .combine='rbind', .packages=c("BSDA", "rankFD", "pseudorank")) %dorng% { #%dorng% 
+        source('ERADE.R')
+        result_part <-result_part_emp
+        p <- as.numeric(par1[i,])
+        mu <- as.numeric(mean[i,])
+        var <- as.numeric(variance[i,])
+        if(dist=="norm") {
+          std <- as.numeric(par2[i,])
+          start <- length(c(p,std))
+          sim.ERADE <- sim_ERADE(N=N, dist=dist, K=1, par1 = p, par2 = std,  nsim=nsim, ar=ar, burnin=burnin)
+          result_part[1,1:start] <- c(p,std)
+        } else {
+          start <- length(p)
+          sim.ERADE <- sim_ERADE(N=N, dist=dist, K=1, par1 = p,   measure= measure, nsim=nsim, ar=ar, burnin=burnin)
+          result_part[1,1:start] <- p
+        }
+        result_part[1,(start+1)] <- mean(sim.ERADE[,5]); result_part[1,(start+2)] <- var(sim.ERADE[,5]) 
+        result_part[1,(start+3)] <- mean(sim.ERADE[,6]); result_part[1,(start+4)] <- var(sim.ERADE[,6]) 
+        
+        result_part[1,(start+5)] <- mean(sim.ERADE[,1])
+        result_part[1,(start+6)] <- var(sim.ERADE[,1])
+        
+        reject_Z <-  sum(sim.ERADE[,2] < 0.05)
+        reject_BM <- sum(sim.ERADE[,3] < 0.05)
+        
+        if(p[1]==p[2]){
+          result_part[1,(start+7)] <- reject_Z/nsim
+          result_part[1,(start+8)] <- reject_BM/nsim
+        } else {
+          result_part[1,(start+7)] <- NA
+          result_part[1,(start+8)] <- NA
+        }
+        result_part[1,(start+9)] <- reject_Z/nsim
+        result_part[1,(start+10)] <- reject_BM/nsim
+        
+        result_part[1,(start+11)] <- mean(sim.ERADE[,4])
+        result_part[1,(start+12)] <- var(sim.ERADE[,4])
+        
+        if(ar=="R_minF"){
+          result_part[1,(start+13)] <- 1- sqrt(p[1])/(sqrt(p[1]) + sqrt(p[2]))
+        } 
+        if(ar=="Neyman"){
+          result_part[1,(start+13)] <- 1 - var[1]/(var[1]+var[2])
+        }
+        result_part[1,(start+14)] <- mean(sim.ERADE[,7]) - result_part[1,(start+13)]
+        
+        result_part
+        
+      }
+      result[,1:(length(colnames))] <- result.ERADE 
+    }
+    if( method == "DBCD"){
+      result_part_emp <- data.frame(matrix(ncol = length(colnames), nrow = 0))
+      result.DBCD <- foreach(i = 1:length(par1[,1]), .combine='rbind', .packages=c("BSDA", "rankFD", "pseudorank")) %dorng% { #%dorng% 
+        source('DBCD.R')
+        result_part <-result_part_emp
+        p <- as.numeric(par1[i,])
+        mu <- as.numeric(mean[i,])
+        var <- as.numeric(variance[i,])
+        if(dist=="norm") {
+          std <- as.numeric(par2[i,])
+          start <- length(c(p,std))
+          sim.DBCD <- sim_DBCD(N=N, dist=dist, K=1, par1 = p, par2 = std,  nsim=nsim, ar=ar, burnin=burnin)
+          result_part[1,1:start] <- c(p,std)
+        } else {
+          start <- length(p)
+          sim.DBCD <- sim_DBCD(N=N, dist=dist, K=1, par1 = p,  measure= measure, nsim=nsim, ar=ar, burnin=burnin)
+          result_part[1,1:start] <- p
+        }
+        result_part[1,(start+1)] <- mean(sim.DBCD[,5]); result_part[1,(start+2)] <- var(sim.DBCD[,5]) 
+        result_part[1,(start+3)] <- mean(sim.DBCD[,6]); result_part[1,(start+4)] <- var(sim.DBCD[,6]) 
+        
+        result_part[1,(start+5)] <- mean(sim.DBCD[,1])
+        result_part[1,(start+6)] <- var(sim.DBCD[,1])
+        
+        reject_Z <-  sum(sim.DBCD[,2] < 0.05)
+        reject_BM <- sum(sim.DBCD[,3] < 0.05)
+        
+        if(p[1]==p[2]){
+          result_part[1,(start+7)] <- reject_Z/nsim
+          result_part[1,(start+8)] <- reject_BM/nsim
+        } else {
+          result_part[1,(start+7)] <- NA
+          result_part[1,(start+8)] <- NA
+        }
+        result_part[1,(start+9)] <- reject_Z/nsim
+        result_part[1,(start+10)] <- reject_BM/nsim
+        
+        result_part[1,(start+11)] <- mean(sim.DBCD[,4])
+        result_part[1,(start+12)] <- var(sim.DBCD[,4])
+        
+        if(ar=="R_minF"){
+          result_part[1,(start+13)] <- 1- sqrt(p[1])/(sqrt(p[1]) + sqrt(p[2]))
+        } 
+        if(ar=="Neyman" ){
+          result_part[1,(start+13)] <- 1 - var[1]/(var[1]+var[2])
+        }
+        result_part[1,(start+14)] <- mean(sim.DBCD[,7]) - result_part[1,(start+13)]
+        
+        result_part
+        
+      }
+      result[,1:(length(colnames))] <- result.DBCD 
+    }
+    
+  } else {
+    print("Unfortunouatly, the multi-armed case has not been implemented yet")
+  }
+  
+  #Print and save Results
+  if(method=="ERADE" || method=="DBCD"){
+    filename <-  paste(method, ar, N, dist,"All.csv",sep="_")
+  } else {
+    filename <-  paste(method, N, dist,"All.csv",sep="_")
+  }
+  
+  write.csv(round(result,4), filename, row.names=TRUE)
+  print(round(result,4))
+  print(round(N*(1-result[7])))
+  stopCluster(cl)
+}
+
+# Generate Parameter Data Frame
+# Bernoulli
+par1_Ber <- data.frame(0.941,0.991)
+
+
+#Scenarios
+N <- c(420)
+burn <- round(N/12)
+Distributions <- c("bern")
+param_list <- expand.grid(N = N, Distributions = Distributions)
+
+
+for(i in 1:dim(param_list)[1]){
+  nsim = 5*10^4 # or 10^4!
+  if(param_list[i,]$Distributions=="bern") {
+    simulation(N=param_list[i,]$N, dist=param_list[i,]$Distributions, K=1, par1 = par1_Ber, burnin=N/2, nsim=nsim, method = "ER", measure = "lrr") #10^4
+    simulation(N=param_list[i,]$N, dist=param_list[i,]$Distributions, K=1, burnin=burn, par1 = par1_Ber, nsim=nsim, method = "SEQ_R_minF", ar="R_minF", measure = "lrr")
+    simulation(N=param_list[i,]$N, dist=param_list[i,]$Distributions, K=1, burnin=burn, par1 = par1_Ber, nsim=nsim, method = "SEQ_Neyman", ar="Neyman", measure = "lrr") #10^4
+    simulation(N=param_list[i,]$N, dist=param_list[i,]$Distributions, K=1, burnin=burn, par1 = par1_Ber, nsim=nsim, method = "DBCD", ar="R_minF", measure = "lrr") #10^4
+    simulation(N=param_list[i,]$N, dist=param_list[i,]$Distributions, K=1, burnin=burn, par1 = par1_Ber, nsim=nsim, method = "DBCD", ar="Neyman", measure = "lrr") #10^4
+    simulation(N=param_list[i,]$N, dist=param_list[i,]$Distributions, K=1, burnin=burn, par1 = par1_Ber, nsim=nsim, method = "ERADE", ar="R_minF", measure = "lrr") #10^4
+    simulation(N=param_list[i,]$N, dist=param_list[i,]$Distributions, K=1, burnin=burn, par1 = par1_Ber, nsim=nsim, method = "ERADE", ar="Neyman", measure = "lrr") #10^4
+  }
+}
